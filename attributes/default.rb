4a81ad43ac9e1c08569d2146a7591bef49837681
Dan Stark <dstark@wingspan.com>
2015-06-15 21:39:49 -0400

    set attribute for bash_file
    This recipe can be used with both /root/.bash_profile or /root/.bashrc.  It will only pass the test with .bashrc, but that shouldn't discourage you from being able to set it to .bash_profile

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,5 @@
+# pattern for datetimeformat
 default['bash_history_timestamp']['flags'] = '%F %T '
+
+# assume this lives under /root
+default['bash_history_timestamp']['bash_file'] = '/root/.bashrc'
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,7 +4,7 @@
 #
 # Copyright (c) 2015 Dan Stark <dstark75@gmail.com>, All Rights Reserved.
 
-file '/root/.bashrc' do
+file node['bash_history_timestamp']['bash_file'] do
   action :create_if_missing
   mode 0400
   owner 'root'
@@ -12,13 +12,13 @@ file '/root/.bashrc' do
 end
 
 replace_or_add 'HISTTIMEFORMAT' do
-  path '/root/.bashrc'
+  path node['bash_history_timestamp']['bash_file']
   pattern 'export HISTTIMEFORMAT*'
   line "export HISTTIMEFORMAT='#{node['bash_history_timestamp']['flags']}'"
   notifies :run, 'bash[reload_shell]', :immediately
 end
 
 bash 'reload_shell' do
-  code "source /root/.bashrc"
+  code "source #{node['bash_history_timestamp']['bash_file']}"
   action :nothing
 end
