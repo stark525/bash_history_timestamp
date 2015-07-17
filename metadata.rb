ad8e95d3c30ecd0f97de84d0aefc51d5318f613f
Dan Stark <dstark@wingspan.com>
2015-07-16 20:58:22 -0400

    adding source_url to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ maintainer_email 'dstark75@gmail.com'
 license 'MIT'
 description 'Adds timestamp to user bash history'
 long_description 'Adds timestamp to .bash_profile, then reloads shell'
+source_url 'https://github.com/stark525/bash_history_timestamp'
 
 version '0.4.1'
 
