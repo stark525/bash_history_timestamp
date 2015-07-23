fc059fda5bfd9803b2c8b956367a7519d5706216
Dan Stark <dstark@wingspan.com>
2015-07-23 08:30:19 -0400

    changing formatting of supports in metadata.rb for supermarket

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license 'MIT'
 description 'Adds timestamp to user bash history'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://github.com/stark525/bash_history_timestamp'
-supports 'centos', '~> 5.10'
+supports 'centos', '>= 5.10'
 
 version '0.4.3'
 
