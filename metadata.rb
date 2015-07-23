6e523483eb4735423eb6b9beb9a3b42239bc60f0
Dan Stark <dstark@wingspan.com>
2015-07-23 08:21:18 -0400

    adding other CentOS versions as officially supported
    Tested with CentOS 5.10, 6.6, and 7.1 base bento (Opscode) boxes and passes all kitchen tests, so adding those to .kitchen.yml and metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,8 @@ license 'MIT'
 description 'Adds timestamp to user bash history'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://github.com/stark525/bash_history_timestamp'
+supports 'centos', '~> 5.10'
 
-version '0.4.2'
+version '0.4.3'
 
 depends 'line', '~> 0.6.1'
